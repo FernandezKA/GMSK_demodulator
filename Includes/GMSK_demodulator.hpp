8d9bdffde60356aff1f0dab409780f8cf309acc4
FernandezKA <fernandes.kir@yandex.ru>
2023-07-08 00:45:17 +0300

    An attempt to get rid of the periodicity of the arc tangent

--- Includes/GMSK_demodulator.hpp
+++ Includes/GMSK_demodulator.hpp
@@ -16,7 +16,8 @@ class GMSK {
 
   std::vector<uint8_t> NRZI_conversion(const std::vector<uint8_t> &) const;
   bool CheckQuadrantSequence(uint8_t last, uint8_t next) const;
-  std::vector<bool> RemoveInsertions(const std::vector<bool>& ) const; 
+  std::vector<bool> RemoveInsertions(const std::vector<bool> &) const;
+
 public:
   GMSK();
   ~GMSK();
@@ -25,16 +26,16 @@ public:
 
   std::vector<bool> Demodulate(void);
   std::vector<std::complex<int16_t>> &LPF(std::vector<std::complex<int16_t>> &);
-  std::vector<double> CumSum(std::vector<double>&, size_t) const; /*Кумуллятивная сумма, с индексом сброса окна*/
+  std::vector<double> MulVec(std::vector<double> &lhs_1, std::vector<double> &lhs_2);
+  std::vector<double> CumSum(std::vector<double> &, size_t)const; /*Кумуллятивная сумма, с индексом сброса окна*/
+  std::vector<double> FiniteDimensions(const std::vector<double> &in) const;
 
 protected:
-
-  const double Fs = 100000; 
-  const double Ts = 10.4e-3; 
-  const size_t baudrate = 9600; 
-  const size_t samples_per_symbol = static_cast<size_t>(Ts*Fs);
-  const size_t frame_size = 0x1000; 
-
+  const double Fs = 100000;
+  const double Ts = 10.4e-3;
+  const size_t baudrate = 9600;
+  const size_t samples_per_symbol = static_cast<size_t>(Ts * Fs);
+  const size_t frame_size = 0x1000;
 };
 
 }; // namespace Demodulators
--- Sources/GMSK_demodulator.cpp
+++ Sources/GMSK_demodulator.cpp
@@ -20,7 +20,10 @@
 #include <cstdint>
 #include <fstream>
 #include <iomanip>
+#include <iterator>
 #include <math.h>
+#include <ostream>
+#include <valarray>
 #include <vector>
 
 namespace Demodulators {
@@ -33,10 +36,11 @@ GMSK::GMSK() {
 GMSK::~GMSK() { std::cout << "GMSK demodulator is removed \n\r"; }
 
 /**
- * @brief Добавление битового потока для обработки. Все дальнейшие операции производятся над ним. 
- * 
+ * @brief Добавление битового потока для обработки. Все дальнейшие операции
+ * производятся над ним.
+ *
  * @param input обрабатываемый битовый поток
- * @return int 
+ * @return int
  */
 int GMSK::Add_Samples(std::vector<std::complex<double>> &input) {
   input_stream.clear();
@@ -48,24 +52,60 @@ int GMSK::Add_Samples(std::vector<std::complex<double>> &input) {
 const std::vector<bool> &GMSK::Get_Bitstream() const { return output_stream; }
 
 /**
- * @brief Имплементация Binary GMSK/FM демодулятора. 
+ * @brief Имплементация Binary GMSK/FM демодулятора.
  * 1. Детектировали фазу у каждого отсчета
- * 2. Проинтегрировали значение в каждом символьном интервале 
- * 3. Сравнили крайнее значение куммулятивной суммы с прошлым временым интервалом. По результату, если 
- * значение фазы увеличилось - 1 бит, иначе 0 бит был передан. 
+ * 2. Проинтегрировали значение в каждом символьном интервале
+ * 3. Сравнили крайнее значение куммулятивной суммы с прошлым временым
+ * интервалом. По результату, если значение фазы увеличилось - 1 бит, иначе 0
+ * бит был передан.
  * @return std::vector<bool> битовый поток после демодуляции
  */
 std::vector<bool> GMSK::Demodulate(void) {
   std::vector<double> phase_samples;
-  for (auto &s : input_stream) {
-    phase_samples.push_back(abs(std::atan2(s.imag(), s.real())));
+  std::ofstream phase("phase.txt");
+
+  std::vector<double> I, Q;
+
+  for (const auto &s : input_stream) {
+    I.push_back(static_cast<double>(s.real()));
+    Q.push_back(static_cast<double>(s.imag()));
   }
 
-  auto cum_sum = CumSum(phase_samples, samples_per_symbol);
-  std::ofstream phase("phase.txt");
-  for (auto s : cum_sum) {
+  std::vector<double> der_re = FiniteDimensions(I);
+  std::vector<double> der_im = FiniteDimensions(Q);
+
+  std::cout<<"Finite dementions size " << der_re.size() << " samples size " << I.size() << std::endl;
+
+  auto mul_re = MulVec(der_re, Q);
+  auto mul_im = MulVec(der_im, I);
+
+  auto re_pow2 = MulVec(I, I);
+  auto im_pow2 = MulVec(Q, Q);
+
+  std::vector<double> w_t; 
+  for(size_t i = 0; i < mul_re.size(); ++i){
+    w_t.push_back((mul_im.at(i) - mul_re.at(i))/(re_pow2.at(i) + im_pow2.at(i))); 
+  }
+
+  auto phase_detected = CumSum(w_t, samples_per_symbol);
+
+  for(auto s : phase_detected){
     phase << s << std::endl;
   }
+  /*
+  Имплементация с арктангенсом сейчас не является рабочей,т.к. не удается
+  корректировать набег фаз, из - за чего демодулируется некорректно.
+  https://ru.dsplib.org/content/signal_fm_demod/signal_fm_demod.html
+  */
+  // for (auto &s : input_stream) {
+  //   phase_samples.push_back(abs(std::atan2(s.imag(), s.real())));
+  // }
+
+  // for (auto s : phase_samples) {
+  //   phase << s << std::endl;
+  // }
+
+  auto cum_sum = CumSum(phase_samples, samples_per_symbol);
 
   for (size_t i = samples_per_symbol - 1; i < phase_samples.size();
        i += samples_per_symbol) {
@@ -79,10 +119,10 @@ std::vector<bool> GMSK::Demodulate(void) {
   return output_stream;
 }
 /**
- * @brief Реализация ФНЧ, который используется перед демодуляцией сигнала. 
- * 
+ * @brief Реализация ФНЧ, который используется перед демодуляцией сигнала.
+ *
  * @param in I/Q отсчеты
- * @return std::vector<std::complex<int16_t>>& Обработанный поток I/Q отсчетов. 
+ * @return std::vector<std::complex<int16_t>>& Обработанный поток I/Q отсчетов.
  */
 std::vector<std::complex<int16_t>> &
 GMSK::LPF(std::vector<std::complex<int16_t>> &in) {
@@ -91,11 +131,12 @@ GMSK::LPF(std::vector<std::complex<int16_t>> &in) {
 }
 
 /**
- * @brief Используется вместо интегратора, для усреднения значений на одном символьном интервале. 
- * 
+ * @brief Используется вместо интегратора, для усреднения значений на одном
+ * символьном интервале.
+ *
  * @param in входные данные
  * @param reset_index размер окна суммирования
- * @return std::vector<double> 
+ * @return std::vector<double>
  */
 std::vector<double> GMSK::CumSum(std::vector<double> &in,
                                  size_t reset_index) const {
@@ -112,11 +153,31 @@ std::vector<double> GMSK::CumSum(std::vector<double> &in,
 }
 
 /**
- * @brief В АИС используется вставка нулевого бита, если мы находим блок последовательных единичек, размером более 5. 
- * В этой функции мы убираем эти вставки, таким образом восстанавливая исходный битовый поток. 
- * TODO: Это не физический уровень! Семантически оно не должно быть тут. 
+ * @brief Данная функция используется для вычисления произодной (первого
+ * порядка, расчет конечных разностей)
+ *
+ * @param in входные данные, для которых мы формируем конечные разности
+ * @return std::vector<double> результирующий вектор после нахождения конечных
+ * разностей
+ */
+std::vector<double>
+GMSK::FiniteDimensions(const std::vector<double> &in) const {
+  std::vector<double> res;
+  res.reserve(in.size());
+  res.push_back(*in.begin());
+  for (auto s = in.begin() + 1; s < in.end(); ++s) {
+    res.push_back(*s - *(s - 1));
+  }
+  return res;
+}
+
+/**
+ * @brief В АИС используется вставка нулевого бита, если мы находим блок
+ * последовательных единичек, размером более 5. В этой функции мы убираем эти
+ * вставки, таким образом восстанавливая исходный битовый поток.
+ * TODO: Это не физический уровень! Семантически оно не должно быть тут.
  * @param in битовый поток, после демодуляции
- * @return std::vector<bool> 
+ * @return std::vector<bool>
  */
 std::vector<bool> GMSK::RemoveInsertions(const std::vector<bool> &in) const {
   std::vector<bool> parsed;
@@ -134,6 +195,17 @@ std::vector<bool> GMSK::RemoveInsertions(const std::vector<bool> &in) const {
     }
   }
   return parsed;
+}
+
+std::vector<double> GMSK::MulVec(std::vector<double> &lhs_1,
+                                   std::vector<double> &lhs_2) {
+  std::vector<double> res;
+  if (lhs_1.size() != lhs_2.size())
+    return res;
+  for (size_t i = 0; i < lhs_1.size(); ++i) {
+    res.push_back(lhs_1.at(i) * lhs_2.at(i));
+  }
+  return res;
 }
 
 } // namespace Demodulators
